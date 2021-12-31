4c215bae0da9acbfe3a1241c8e5d4762a8cd7079
hikari97 <h.makki97@gmail.com>
2021-12-31 21:57:18 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { themes } from "./theme";
 import { GlobalStyles } from "./global";
-import { settings } from "./portfolio";
-import ReactGA from "react-ga";
 
 function App() {
-  // useEffect(() => {
-  //   if (settings.googleTrackingID) {
-  //     ReactGA.initialize(settings.googleTrackingID, {
-  //       testMode: process.env.NODE_ENV === "test",
-  //     });
-  //     ReactGA.pageview(window.location.pathname + window.location.search);
-  //   }
-  // }, []);
-
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
 
   return (
