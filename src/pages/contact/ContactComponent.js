163ea3f06818124e3b8e17b7dfdc93f46696852e
hikari97 <h.makki97@gmail.com>
2021-12-31 21:47:08 +0800

    up

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -70,7 +70,7 @@ function Contact(props) {
               </p>
               <div className="blogsite-btn-div">
                 <a {...styles} className="general-btn" href={blogSection.link}>
-                  Facebook
+                  WhatsApp
                 </a>
               </div>
             </div>
